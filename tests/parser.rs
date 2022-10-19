c0b50d025b0b21bc7a7f0b794c19864e347d6edb
Christoph Heiss <christoph.heiss@cybertec.at>
2022-10-19 13:25:02 +0200

    tests(coverage): Add tests for parsing simple SQL `SELECT` statements
    Signed-off-by: Christoph Heiss <christoph.heiss@cybertec.at>

--- tests/parser.rs
+++ tests/parser.rs
@@ -2,7 +2,7 @@
 // SPDX-FileCopyrightText: 2022 CYBERTEC PostgreSQL International GmbH
 // <office@cybertec.at>
 
-use poc_plpgsql_analyzer::{parse_function, parse_procedure};
+use poc_plpgsql_analyzer::{parse_function, parse_procedure, parse_select};
 use std::fs;
 use std::path::Path;
 
@@ -34,6 +34,20 @@ fn test_parse_function_coverage(path: &Path) -> datatest_stable::Result<()> {
     Ok(())
 }
 
+fn test_parse_dql_select_coverage(path: &Path) -> datatest_stable::Result<()> {
+    let input = fs::read_to_string(path)?;
+    let result = parse_select(&input);
+    assert!(result.is_ok(), "{:#?}", result);
+    let parse = result.unwrap();
+    assert!(
+        parse.errors.is_empty(),
+        "\n{}\n{:?}",
+        parse.tree(),
+        parse.errors
+    );
+    Ok(())
+}
+
 datatest_stable::harness!(
     test_parse_procedure_coverage,
     "tests/procedure",
@@ -41,4 +55,7 @@ datatest_stable::harness!(
     test_parse_function_coverage,
     "tests/function",
     r"^(.*).ora\.sql$",
+    test_parse_dql_select_coverage,
+    "tests/dql",
+    r"select_(.*).ora\.sql$"
 );
