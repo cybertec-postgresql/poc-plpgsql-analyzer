218c31daebef69eb51fd183d50dd246149eb39c8
Christoph Heiss <christoph.heiss@cybertec.at>
2022-11-07 15:56:11 +0100

    fix(grammar): Also allow either `IS` or `AS` in function declaration
    Signed-off-by: Christoph Heiss <christoph.heiss@cybertec.at>

--- src/grammar/function.rs
+++ src/grammar/function.rs
@@ -50,7 +50,7 @@ fn parse_attributes(p: &mut Parser) {
 
 /// Parses the body of a function.
 fn parse_body(p: &mut Parser) {
-    p.expect(TokenKind::IsKw);
+    p.expect_one_of(&[TokenKind::IsKw, TokenKind::AsKw]);
     p.expect(TokenKind::BeginKw);
     p.eat_ws();
 
