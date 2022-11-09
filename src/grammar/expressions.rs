35fd49a034d024e191cb59ac026d0f6f9433bfc1
KieranKaelin <kieran_kaelin@yahoo.co.uk>
2022-11-09 13:39:02 +0100

    tests(expr): Add test for nested expressions

--- src/grammar/expressions.rs
+++ src/grammar/expressions.rs
@@ -92,39 +92,104 @@ Root@0..75
     Whitespace@7..8 " "
     Keyword@8..11 "AND"
     Whitespace@11..12 " "
-    LParen@12..13 "("
-    Integer@13..15 "10"
-    Whitespace@15..16 " "
-    ComparisonOp@16..18 "<>"
-    Whitespace@18..19 " "
-    Ident@19..20 "b"
-    Whitespace@20..21 " "
-    Keyword@21..23 "OR"
-    Whitespace@23..24 " "
-    LParen@24..25 "("
-    Ident@25..26 "c"
-    Whitespace@26..27 " "
-    ComparisonOp@27..28 "="
-    Whitespace@28..29 " "
-    QuotedLiteral@29..34 "'foo'"
-    Whitespace@34..35 " "
-    Keyword@35..38 "AND"
-    Whitespace@38..39 " "
-    Ident@39..42 "bar"
-    Whitespace@42..43 " "
-    ComparisonOp@43..45 ">="
-    Whitespace@45..46 " "
-    Integer@46..48 "42"
-    RParen@48..49 ")"
-    Whitespace@49..50 " "
-    Keyword@50..53 "AND"
-    Whitespace@53..54 " "
-    Ident@54..57 "foo"
-    Whitespace@57..58 " "
-    ComparisonOp@58..63 "ILIKE"
-    Whitespace@63..64 " "
-    QuotedLiteral@64..74 "'%stonks%'"
-    RParen@74..75 ")"
+    Expression@12..75
+      LParen@12..13 "("
+      Integer@13..15 "10"
+      Whitespace@15..16 " "
+      ComparisonOp@16..18 "<>"
+      Whitespace@18..19 " "
+      Ident@19..20 "b"
+      Whitespace@20..21 " "
+      Keyword@21..23 "OR"
+      Whitespace@23..24 " "
+      Expression@24..49
+        LParen@24..25 "("
+        Ident@25..26 "c"
+        Whitespace@26..27 " "
+        ComparisonOp@27..28 "="
+        Whitespace@28..29 " "
+        QuotedLiteral@29..34 "'foo'"
+        Whitespace@34..35 " "
+        Keyword@35..38 "AND"
+        Whitespace@38..39 " "
+        Ident@39..42 "bar"
+        Whitespace@42..43 " "
+        ComparisonOp@43..45 ">="
+        Whitespace@45..46 " "
+        Integer@46..48 "42"
+        RParen@48..49 ")"
+      Whitespace@49..50 " "
+      Keyword@50..53 "AND"
+      Whitespace@53..54 " "
+      Ident@54..57 "foo"
+      Whitespace@57..58 " "
+      ComparisonOp@58..63 "ILIKE"
+      Whitespace@63..64 " "
+      QuotedLiteral@64..74 "'%stonks%'"
+      RParen@74..75 ")"
+"#]],
+        );
+    }
+
+    #[test]
+    fn test_parse_unbalanced_rparen() {
+        check(
+            parse("(a < 100))", parse_expr),
+            expect![[r#"
+Root@0..38
+  Expression@0..9
+    LParen@0..1 "("
+    Ident@1..2 "a"
+    Whitespace@2..3 " "
+    ComparisonOp@3..4 "<"
+    Whitespace@4..5 " "
+    Integer@5..8 "100"
+    RParen@8..9 ")"
+  Error@9..38
+    Text@9..38 "Incomplete input; unp ..."
+"#]],
+        );
+    }
+
+    #[test]
+    fn test_parse_unbalanced_lparen() {
+        check(
+            parse("(a < 100", parse_expr),
+            expect![[r#"
+Root@0..44
+  Expression@0..44
+    LParen@0..1 "("
+    Ident@1..2 "a"
+    Whitespace@2..3 " "
+    ComparisonOp@3..4 "<"
+    Whitespace@4..5 " "
+    Integer@5..8 "100"
+    Error@8..44
+      Text@8..44 "Unbalanced pair of pa ..."
+"#]],
+        );
+    }
+
+    #[test]
+    fn test_parse_nested_paren() {
+        check(
+            parse("(((a < 100)))", parse_expr),
+            expect![[r#"
+Root@0..13
+  Expression@0..13
+    LParen@0..1 "("
+    Expression@1..12
+      LParen@1..2 "("
+      Expression@2..11
+        LParen@2..3 "("
+        Ident@3..4 "a"
+        Whitespace@4..5 " "
+        ComparisonOp@5..6 "<"
+        Whitespace@6..7 " "
+        Integer@7..10 "100"
+        RParen@10..11 ")"
+      RParen@11..12 ")"
+    RParen@12..13 ")"
 "#]],
         );
     }
