edf115802f8053806e38e1fb52b58d747025e568
Christoph Heiss <christoph.heiss@cybertec.at>
2022-11-16 14:42:55 +0100

    chore(tests): Add `prettier` and format test code
    Signed-off-by: Christoph Heiss <christoph.heiss@cybertec.at>

--- tests/typescript/tests/analyzer.ts
+++ tests/typescript/tests/analyzer.ts
@@ -10,11 +10,12 @@ const PROCEDURE_HEADINGS_DIR = '../procedure/heading';
 const QUERYS_DIR = '../dql';
 
 describe('try to parse and analyze Oracle function', () => {
-  const files = fs.readdirSync(FUNCTION_HEADINGS_DIR)
-    .filter((name) => name.match(/^(.+)\.ora\.sql$/))
-    .map((name) => path.join(FUNCTION_HEADINGS_DIR, name));
+  const files = fs
+    .readdirSync(FUNCTION_HEADINGS_DIR)
+    .filter(name => name.match(/^(.+)\.ora\.sql$/))
+    .map(name => path.join(FUNCTION_HEADINGS_DIR, name));
 
-  it.each(files)('should parse %s successfully', (path) => {
+  it.each(files)('should parse %s successfully', path => {
     const content = fs.readFileSync(path, 'utf8');
     const metaData = analyze(DboType.Function, content, { tables: {} });
 
@@ -46,11 +47,12 @@ describe('try to parse and analyze Oracle function', () => {
 });
 
 describe('try to parse and analyze Oracle procedures', () => {
-  const files = fs.readdirSync(PROCEDURE_HEADINGS_DIR)
-    .filter((name) => name.match(/^(.+)\.ora\.sql$/))
-    .map((name) => path.join(PROCEDURE_HEADINGS_DIR, name));
+  const files = fs
+    .readdirSync(PROCEDURE_HEADINGS_DIR)
+    .filter(name => name.match(/^(.+)\.ora\.sql$/))
+    .map(name => path.join(PROCEDURE_HEADINGS_DIR, name));
 
-  it.each(files)('should parse %s successfully', (path) => {
+  it.each(files)('should parse %s successfully', path => {
     const content = fs.readFileSync(path, 'utf8');
     const metaData = analyze(DboType.Procedure, content, { tables: {} });
 
@@ -82,11 +84,12 @@ describe('try to parse and analyze Oracle procedures', () => {
 });
 
 describe('try to parse and analyze Oracle `SELECT` querys', () => {
-  const files = fs.readdirSync(QUERYS_DIR)
-    .filter((name) => name.match(/^(.+)\.ora\.sql$/))
-    .map((name) => path.join(QUERYS_DIR, name));
+  const files = fs
+    .readdirSync(QUERYS_DIR)
+    .filter(name => name.match(/^(.+)\.ora\.sql$/))
+    .map(name => path.join(QUERYS_DIR, name));
 
-  it.each(files)('should parse %s successfully', (path) => {
+  it.each(files)('should parse %s successfully', path => {
     const content = fs.readFileSync(path, 'utf8');
     const metaData = analyze(DboType.Query, content, { tables: {} });
 
@@ -108,45 +111,45 @@ describe('try to parse and analyze Oracle `SELECT` querys', () => {
 });
 
 describe('passing type context information into analyzer', () => {
-    it('should be able to analyze procedure with `%TYPE` parameters', () => {
-      const content = fs.readFileSync('../fixtures/log_last_login_fuzzy.ora.sql','utf8');
-      const context = {
-        tables: {
-          persons: {
-            columns: {
-              // TODO: This should be usable as `id: { typ: DboColumnType.Integer }`
-              // But somehow `serde_wasw_bindgen` messes up and can only deserialize
-              // this type from strings.
-              id: { typ: 'Integer' },
-              name: { typ: 'Text' },
-              number_of_logins: { typ: 'Integer' },
-              last_login: { typ: 'Date' },
-            },
+  it('should be able to analyze procedure with `%TYPE` parameters', () => {
+    const content = fs.readFileSync('../fixtures/log_last_login_fuzzy.ora.sql', 'utf8');
+    const context = {
+      tables: {
+        persons: {
+          columns: {
+            // TODO: This should be usable as `id: { typ: DboColumnType.Integer }`
+            // But somehow `serde_wasw_bindgen` messes up and can only deserialize
+            // this type from strings.
+            id: { typ: 'Integer' },
+            name: { typ: 'Text' },
+            number_of_logins: { typ: 'Integer' },
+            last_login: { typ: 'Date' },
           },
         },
-      };
+      },
+    };
 
-      const metaData = analyze(DboType.Procedure, content, context);
-      expect(metaData.procedure.name).toEqual('log_last_login_fuzzy');
-      expect(metaData.procedure.linesOfCode).toEqual(2);
+    const metaData = analyze(DboType.Procedure, content, context);
+    expect(metaData.procedure.name).toEqual('log_last_login_fuzzy');
+    expect(metaData.procedure.linesOfCode).toEqual(2);
 
-      expect(metaData.rules).toBeInstanceOf(Array);
-      expect(metaData.rules.length).toEqual(2);
-      expect(metaData.rules[0]).toEqual({
-        name: 'CYAR-0002',
-        locations: [ { offset: { start: 315, end: 317 } } ],
-        short_desc: 'Replace procedure prologue',
-      });
-      expect(content.substring(315, 317)).toEqual('IS');
+    expect(metaData.rules).toBeInstanceOf(Array);
+    expect(metaData.rules.length).toEqual(2);
+    expect(metaData.rules[0]).toEqual({
+      name: 'CYAR-0002',
+      locations: [{ offset: { start: 315, end: 317 } }],
+      short_desc: 'Replace procedure prologue',
+    });
+    expect(content.substring(315, 317)).toEqual('IS');
 
-      expect(metaData.rules[1]).toEqual({
-        name: 'CYAR-0003',
-        locations: [ { offset: { start: 571, end: 595 } } ],
-        short_desc: 'Replace procedure epilogue',
-      });
-      expect(content.substring(571, 595)).toEqual('END log_last_login_fuzzy');
+    expect(metaData.rules[1]).toEqual({
+      name: 'CYAR-0003',
+      locations: [{ offset: { start: 571, end: 595 } }],
+      short_desc: 'Replace procedure epilogue',
+    });
+    expect(content.substring(571, 595)).toEqual('END log_last_login_fuzzy');
 
     expect(metaData.function).toBeUndefined();
     expect(metaData.query).toBeUndefined();
-    });
+  });
 });
