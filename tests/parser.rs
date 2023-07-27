f1074186159ddde08add0048a29abd30ba0d22b3
KieranKaelin <kieran_kaelin@yahoo.co.uk>
2023-07-27 15:00:20 +0200

    refactor(parser): deduplicate parser coverage tests
    Signed-off-by: KieranKaelin <kieran_kaelin@yahoo.co.uk>

--- tests/parser.rs
+++ tests/parser.rs
@@ -1,43 +1,31 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE.md
-// SPDX-FileCopyrightText: 2022 CYBERTEC PostgreSQL International GmbH
+// SPDX-FileCopyrightText: 2023 CYBERTEC PostgreSQL International GmbH
 // <office@cybertec.at>
 
-use poc_plpgsql_analyzer::{parse_function, parse_procedure, parse_query};
 use std::fs;
 use std::path::Path;
 
-fn test_parse_procedure_coverage(path: &Path) -> datatest_stable::Result<()> {
-    let input = fs::read_to_string(path)?;
-    let result = parse_procedure(&input);
-    assert!(result.is_ok(), "{:#?}", result);
-    let parse = result.unwrap();
-    assert!(
-        parse.errors.is_empty(),
-        "\n{:#?}\n{:?}",
-        parse.syntax(),
-        parse.errors
-    );
-    Ok(())
-}
+use poc_plpgsql_analyzer::{
+    parse_function, parse_procedure, parse_query, parse_trigger, parse_view,
+};
 
-fn test_parse_function_coverage(path: &Path) -> datatest_stable::Result<()> {
-    let input = fs::read_to_string(path)?;
-    let result = parse_function(&input);
-    assert!(result.is_ok(), "{:#?}", result);
-    let parse = result.unwrap();
-    assert!(
-        parse.errors.is_empty(),
-        "\n{:#?}\n{:?}",
-        parse.syntax(),
-        parse.errors
-    );
-    Ok(())
-}
+fn test_parse_coverage(path: &Path) -> datatest_stable::Result<()> {
+    let components = path.components().collect::<Vec<_>>();
+    let typ = components
+        .get(1)
+        .expect("Failed to get second component from path");
+    let content = fs::read_to_string(path)?;
 
-fn test_parse_dql_select_coverage(path: &Path) -> datatest_stable::Result<()> {
-    let input = fs::read_to_string(path)?;
-    let result = parse_query(&input);
+    let result = match typ.as_os_str().to_str().unwrap() {
+        "dql" => parse_query(&content),
+        "function" => parse_function(&content),
+        "procedure" => parse_procedure(&content),
+        "trigger" => parse_trigger(&content),
+        "view" => parse_view(&content),
+        typ => panic!("Can not parse typ {}", typ),
+    };
     assert!(result.is_ok(), "{:#?}", result);
+
     let parse = result.unwrap();
     assert!(
         parse.errors.is_empty(),
@@ -49,13 +37,19 @@ fn test_parse_dql_select_coverage(path: &Path) -> datatest_stable::Result<()> {
 }
 
 datatest_stable::harness!(
-    test_parse_procedure_coverage,
+    test_parse_coverage,
     "tests/procedure",
     r"^(.*).ora\.sql$",
-    test_parse_function_coverage,
+    test_parse_coverage,
     "tests/function",
-    r"^(.*).ora\.sql$",
-    test_parse_dql_select_coverage,
+    r"^(.*)\.sql$",
+    test_parse_coverage,
     "tests/dql",
-    r"(.*).ora\.sql$"
+    r"(.*)\.sql$",
+    test_parse_coverage,
+    "tests/trigger",
+    r"(.*)\.sql$",
+    test_parse_coverage,
+    "tests/view",
+    r"(.*)\.sql$"
 );
