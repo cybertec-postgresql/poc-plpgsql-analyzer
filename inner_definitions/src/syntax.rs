1a4cb0fc25c12dd3196965ec13caaf178c29125e
if-loop69420 <j.sztavi@pm.me>
2024-07-30 10:20:42 +0200

    Reformed the crate structure of inner_definitions. Now migrating source generation functions from inner_definitions to inner_source_gen

--- inner_definitions/src/syntax.rs
+++ inner_definitions/src/syntax.rs
@@ -4,115 +4,109 @@
 
 //! Generates the [`crate::SyntaxKind`] enum and the mapping of the [`crate::TokenKind`] enum
 
-#[cfg(test)]
-pub mod lib {
-    use heck::ToUpperCamelCase;
-    use proc_macro2::{Ident, TokenStream};
-    use quote::{format_ident, quote};
+use heck::ToUpperCamelCase;
+use proc_macro2::{Ident, TokenStream};
+use quote::{format_ident, quote};
 
-    use crate::sourcegen::data::lib::SYNTAX_NODES;
-    use crate::sourcegen::data::lib::TOKENS;
-    use crate::sourcegen::lib::{
-        add_preamble, guarantee_file_content, project_path, rustfmt_content,
+use crate::data::SYNTAX_NODES;
+use crate::data::TOKENS;
+use crate::{add_preamble, guarantee_file_content, project_path, rustfmt_content};
+
+pub struct SyntaxNode<'a> {
+    pub name: &'a str,
+    pub explanation: &'a str,
+    pub tokens: &'a [&'a str],
+}
+
+impl SyntaxNode<'_> {
+    fn to_ident(&self) -> Ident {
+        format_ident!("{}", self.name.to_upper_camel_case(),)
+    }
+
+    fn to_enum_variant(&self) -> TokenStream {
+        let ident = self.to_ident();
+        let doc = self.explanation;
+        quote! {
+            #[doc = #doc]
+            #ident,
+        }
+    }
+}
+
+macro_rules! S {
+    ($name:literal, $explanation:literal) => {
+        SyntaxNode {
+            name: $name,
+            explanation: $explanation,
+            tokens: &[],
+        }
     };
+    ($name:literal, $explanation:literal, $tokens:expr) => {
+        SyntaxNode {
+            name: $name,
+            explanation: $explanation,
+            tokens: $tokens,
+        }
+    };
+}
+pub(crate) use S;
 
-    pub struct SyntaxNode<'a> {
-        pub name: &'a str,
-        pub explanation: &'a str,
-        pub tokens: &'a [&'a str],
-    }
+fn sourcegen_syntax() {
+    let file = project_path().join("src/syntax/generated.rs");
+    let content = rustfmt_content(add_preamble(file!(), generate_content()));
+    guarantee_file_content(&file, content.as_str());
+}
 
-    impl SyntaxNode<'_> {
-        fn to_ident(&self) -> Ident {
-            format_ident!("{}", self.name.to_upper_camel_case(),)
+fn generate_content() -> String {
+    // All variants of the `SyntaxKind` enum
+    let syntax_nodes: TokenStream = SYNTAX_NODES.iter().map(|t| t.to_enum_variant()).collect();
+
+    let tokens: TokenStream = TOKENS
+        .iter()
+        .map(|t| {
+            let token = t.to_ident();
+            let syntax = if let Some(syntax_kind) = t.syntax_kind {
+                format_ident!("{}", syntax_kind.to_upper_camel_case())
+            } else {
+                format_ident!("Keyword")
+            };
+            quote! { TokenKind::#token => SyntaxKind::#syntax, }
+        })
+        .collect();
+
+    let content = quote! {
+        use num_derive::{FromPrimitive, ToPrimitive};
+        use num_traits::ToPrimitive;
+
+        use crate::lexer::TokenKind;
+
+        /// Represents all possible kind of syntax items the parser can process.
+        ///
+        /// Examples
+        /// * <https://blog.kiranshila.com/blog/easy_cst.md>
+        /// * <https://arzg.github.io/lang/10/>
+        /// * <https://github.com/rust-analyzer/rowan/blob/master/examples/s_expressions.rs>
+        #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Ord, PartialOrd, FromPrimitive, ToPrimitive)]
+        #[repr(u16)]
+        pub enum SyntaxKind {
+            #syntax_nodes
         }
 
-        fn to_enum_variant(&self) -> TokenStream {
-            let ident = self.to_ident();
-            let doc = self.explanation;
-            quote! {
-                #[doc = #doc]
-                #ident,
+        impl From<SyntaxKind> for rowan::SyntaxKind {
+            fn from(kind: SyntaxKind) -> Self {
+                rowan::SyntaxKind(kind.to_u16().unwrap())
             }
         }
-    }
 
-    macro_rules! S {
-        ($name:literal, $explanation:literal) => {
-            SyntaxNode {
-                name: $name,
-                explanation: $explanation,
-                tokens: &[],
-            }
-        };
-        ($name:literal, $explanation:literal, $tokens:expr) => {
-            SyntaxNode {
-                name: $name,
-                explanation: $explanation,
-                tokens: $tokens,
-            }
-        };
-    }
-    pub(crate) use S;
-
-    #[test]
-    fn sourcegen_syntax() {
-        let file = project_path().join("src/syntax/generated.rs");
-        let content = rustfmt_content(add_preamble(file!(), generate_content()));
-        guarantee_file_content(&file, content.as_str());
-    }
-
-    fn generate_content() -> String {
-        // All variants of the `SyntaxKind` enum
-        let syntax_nodes: TokenStream = SYNTAX_NODES.iter().map(|t| t.to_enum_variant()).collect();
-
-        let tokens: TokenStream = TOKENS
-            .iter()
-            .map(|t| {
-                let token = t.to_ident();
-                let syntax = if let Some(syntax_kind) = t.syntax_kind {
-                    format_ident!("{}", syntax_kind.to_upper_camel_case())
-                } else {
-                    format_ident!("Keyword")
-                };
-                quote! { TokenKind::#token => SyntaxKind::#syntax, }
-            })
-            .collect();
-
-        let content = quote! {
-            use num_derive::{FromPrimitive, ToPrimitive};
-            use num_traits::ToPrimitive;
-
-            use crate::lexer::TokenKind;
-
-            /// Represents all possible kind of syntax items the parser can process.
-            ///
-            /// Examples
-            /// * <https://blog.kiranshila.com/blog/easy_cst.md>
-            /// * <https://arzg.github.io/lang/10/>
-            /// * <https://github.com/rust-analyzer/rowan/blob/master/examples/s_expressions.rs>
-            #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Ord, PartialOrd, FromPrimitive, ToPrimitive)]
-            #[repr(u16)]
-            pub enum SyntaxKind {
-                #syntax_nodes
-            }
-
-            impl From<SyntaxKind> for rowan::SyntaxKind {
-                fn from(kind: SyntaxKind) -> Self {
-                    rowan::SyntaxKind(kind.to_u16().unwrap())
+        impl From<TokenKind> for SyntaxKind {
+            fn from(kind: TokenKind) -> Self {
+                match kind {
+                    #tokens
+                    TokenKind::Error => SyntaxKind::Error,
+                    TokenKind::Eof => unreachable!(),
                 }
             }
-
-            impl From<TokenKind> for SyntaxKind {
-                fn from(kind: TokenKind) -> Self {
-                    match kind {
-                        #tokens
-                        TokenKind::Error => SyntaxKind::Error,
-                        TokenKind::Eof => unreachable!(),
-                    }
-                }
-            }
-        };
-        content.to_string()
-    }
+        }
+    };
+    content.to_string()
 }
