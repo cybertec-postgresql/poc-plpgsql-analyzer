5c70ab9b8e411c7261e492b6adc9c4796921cbec
if-loop69420 <j.sztavi@pm.me>
2024-07-31 10:44:36 +0200

    Put prior and connect_by_root into one line in expressions

--- src/grammar/expressions.rs
+++ src/grammar/expressions.rs
@@ -200,8 +200,7 @@ impl Operator {
 fn prefix_bp(op: TokenKind) -> Option<Operator> {
     Some(match op {
         T![not] => Operator::new_with_map(5, SyntaxKind::LogicOp),
-        T![prior] => Operator::new_with_map(17, SyntaxKind::LogicOp),
-        T![connect_by_root] => Operator::new_with_map(17, SyntaxKind::LogicOp),
+        T![prior] | T![connect_by_root] => Operator::new_with_map(17, SyntaxKind::LogicOp),
         T![+] | T![-] => Operator::new_plain(17),
         _ => return None,
     })
