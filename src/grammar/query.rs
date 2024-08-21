46bb4ec6b037dec6934c224b74374953689dfc00
Jeremy Sztavinovszki <81078181+if-loop69420@users.noreply.github.com>
2024-08-21 10:54:58 +0200

    Update src/grammar/query.rs
    Co-authored-by: Kieran Kaelin <kieran_kaelin@yahoo.co.uk>

--- src/grammar/query.rs
+++ src/grammar/query.rs
@@ -1456,18 +1456,8 @@ Root@0..61
     in_customer_id NUMBER 
 )
 IS
-  r_contact contacts%ROWTYPE;
 BEGIN
-  -- get contact based on customer id
-  SELECT *
-  INTO r_contact
-  FROM contacts
-  WHERE customer_id = p_customer_id;
-
-  -- print out contact's information
-  dbms_output.put_line( r_contact.first_name || ' ' ||
-  r_contact.last_name || '<' || r_contact.email ||'>' );
-
+  NULL;
 END; 
 SELECT * from employee;",
                 parse_cte,
